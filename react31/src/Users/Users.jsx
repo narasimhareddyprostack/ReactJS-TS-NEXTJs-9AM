bfdcf0ad31f3ee5e13fd7dfa1c8b5f6464943247
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-08-20 12:01:31 +0530

    Ts

--- react31/src/Users/Users.jsx
+++ react31/src/Users/Users.jsx
@@ -1,49 +1,48 @@
-import React from "react";
-import Axios from 'axios'
-class Users extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={users:[]}
-    }
-   componentDidMount(){  
-        Axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((resp)=>{
-            this.setState({users:resp.data})
-        })
-        .catch(()=>{})
-    }
-    render(){
-        return <div className="container">
-                        <div className="row">
-                            <div className="col-6">
-                            <h4>Users Component</h4>
-                    <pre>{JSON.stringify(this.state.users)}</pre>
-                   
-                    <table className="table">
-                        <thead>
-                        <tr>
-                            <th>User Id</th>
-                            <th>User Name</th>
-                            <th>User Email</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            this.state.users.map((user)=>{
-                                return <tr key={user.id}>
-                                            <td>{user.id}</td>
-                                            <td>{user.username}</td>
-                                            <td>{user.email}</td>
-                                        </tr>
-                            })
-                        }
-                        </tbody>
-                    </table>
-                        </div>
-                        </div>
-                    
-                </div>
-    }
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => {
+        setUsers(resp.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching users:", err);
+      });
+  }, []); // empty dependency → runs once like componentDidMount
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h4>Users Component</h4>
+          <pre>{JSON.stringify(users)}</pre>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th>User Id</th>
+                <th>User Name</th>
+                <th>User Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Users;
