6dd1a5c0d356205936404d418b56fcc385210a15
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-08-22 05:21:11 +0530

    tst

---
+++ react31/src/Users/Users-functional.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    Axios.get("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => {
+        setUsers(resp.data);
+      })
+      .catch((err) => { console.error("Error fetching users:", err);});
+   }, []); // empty dependency → runs once like componentDidMount
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+        <h4>Users Component</h4><pre>{JSON.stringify(users)}</pre>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>User Id</th>
+                <th>User Name</th>
+                <th>User Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Users;
--- react31/src/Users/Users.jsx
+++ react31/src/Users/Users.jsx
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Axios from "axios";
-
-function Users() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  users: [],};
+  }
+  componentDidMount() {
     Axios.get("https://jsonplaceholder.typicode.com/users")
       .then((resp) => {
-        setUsers(resp.data);
+        this.setState({ users: resp.data });
       })
-      .catch((err) => {
-        console.error("Error fetching users:", err);
-      });
-  }, []); // empty dependency → runs once like componentDidMount
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">
-          <h4>Users Component</h4>
-          <pre>{JSON.stringify(users)}</pre>
-
-          <table className="table">
-            <thead>
-              <tr>
-                <th>User Id</th>
-                <th>User Name</th>
-                <th>User Email</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.username}</td>
-                  <td>{user.email}</td>
+      .catch((err) => {console.error("Error fetching users:", err);});
+  }
+  render() {
+    const { users } = this.state;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <h4>Users Component</h4>
+            <pre>{JSON.stringify(users)}</pre>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>User Id</th>
+                  <th>User Name</th>
+                  <th>User Email</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Users;
